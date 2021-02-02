3127552acaf8add2c9838724ad58562cd4a511c5
Anamta Amin <anamtaamin35@gmail.com>
2021-02-03 01:11:09 +0530

    Update Cards.jsx

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -11,7 +11,6 @@ const Cards=(props)=>{
                 <img src={props.imgsrc} class="card-img-top" alt={props.imgsrc}/>
                     <div class="card-body">
                         <h5 class="card-title font-weight-bold">{props.title}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <NavLink to="./Contact" className="btn btn-primary">Contact us</NavLink>
                     </div>
             </div>
